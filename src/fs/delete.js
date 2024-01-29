e9af3e3c4d5fb75a0a5037f0406f597a6c4d2557
Siarhei Kuchko <siarheikuchko@gmail.com>
2024-01-30 00:32:21 +0200

    feat: add solution for delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+    const pathToFile = './files/fileToRemove.txt';    
+    fs.unlink(pathToFile, (err) => {
+        if (err) {
+            console.log('FS operation failed');
+            return;
+        } else {console.log('File deleted successfully')};
+    }); 
 };
 
 await remove();
