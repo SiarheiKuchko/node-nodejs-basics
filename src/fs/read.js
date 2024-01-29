9036e9c3539aa551b87df428ceeb558e599deed9
Siarhei Kuchko <siarheikuchko@gmail.com>
2024-01-29 23:20:31 +0200

    feat: add solution for read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'fs';
+
+const read = async () => {    
+    
+    fs.readFile('./files/fileToRead.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('FS operation failed');
+            return;
+        }
+        console.log(data);
+    });
+
 };
 
 await read();
